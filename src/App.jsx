2eaee27189467a0f19f4f7f123c8ca3a82b03dca
danieldagerom <danielprimo9@gmail.com>
2023-02-13 20:52:40 -0300

    Adiciona estilos de sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <div>
       <Header />
-
-      <Post 
-        author="Daniel Primo" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit veniam unde, vero quasi repellendus corrupti aspernatur quis consectetur, eveniet porro saepe animi tempora cupiditate atque alias tempore ea! Dolores, architecto?"
-      />
-      <Post 
-        author="Marcos Paulo"
-        content="Melhor morrer do que perder a vida!"
-      />
-      <Post />
-      <Post />
+        <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            <Post 
+              author="Daniel Primo" 
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit veniam unde, vero quasi repellendus corrupti aspernatur quis consectetur, eveniet porro saepe animi tempora cupiditate atque alias tempore ea! Dolores, architecto?"
+            />
+            <Post 
+              author="Marcos Paulo"
+              content="Melhor morrer do que perder a vida!"
+            />
+          </main>
+        </div>
     </div>
-    
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return(
+        <aside className={styles.sidebar} >
+            <img  
+            className={styles.cover}
+            src="https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
+
+            <div className={styles.profile}>
+                <strong>Daniel Primo</strong>
+                <span>Web Developer</span>
+
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+
+        
+    );
+}
