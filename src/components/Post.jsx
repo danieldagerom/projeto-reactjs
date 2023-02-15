8737c7405f3bf93a9fbbb03457b1531d6a8ee1b2
danieldagerom <danielprimo9@gmail.com>
2023-02-15 15:44:46 -0300

    Adiciona estilos do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,9 +6,11 @@ export function Post(){
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://github.com/danieldagerom.png" />
-                    <div className={styles.authorInfo}></div>
-                    <strong>Daniel Primo</strong>
-                    <span>Web Developer</span>
+                    <div className={styles.authorInfo}>
+                        <strong>Daniel Primo</strong>
+                        <span>Web Developer</span>
+                    </div>
+                    
                 </div>
                 
                 <time title='11 de Maio, as 8:13h' dateTime='2022-05-11 08:13:40'>Publicado há 1 hora</time>
@@ -17,8 +19,12 @@ export function Post(){
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 jane.design/doctorcare</p>
-                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+                <p>👉 {' '}<a href="">jane.design/doctorcare</a></p>
+                <p> 
+                    <a>#novoprojeto </a>{' '}
+                    <a>#nlw </a>{' '}
+                    <a>#rocketseat</a>
+                </p>
             </div>
         </article>
     )
